b930c2f431f81de59ef11eca6030af5e8c26d7bd
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2025-07-24 18:31:48 +0530

    domain name in cors

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://www.kinaav.in',
+  credentials: true
+}));
 app.use(express.json());
 
-// Connect to MongoDB (replace with your connection string)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
